fc9c476aecf1286dc2393e8b88c9ad19fd14ff22
viktort <viktor.trako@cazoo.co.uk>
2021-02-02 15:39:43 +0000

    🟢🟢🟢🟢 GREEN -  - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,8 @@
-import { sum } from './index';
 import { expect } from 'chai';
+
+export function sum(a: number, b: number): number {
+    return a + b;
+}
 
 describe('sample test-suite', () => {
     it('sum 1 + 3 should be 4', () => {
