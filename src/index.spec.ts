7071b5dae92e0da85a4404bb2d89a33f0e61ade4
Chris Harding <chris.harding@cazoo.co.uk>
2021-02-09 15:33:59 +0000

    empty repo

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,31 +1,7 @@
 import { expect } from 'chai';
 
-function sum(a: number, b: number): number {
-    return a + b;
-}
-
-function fizzbuzz(a: number): string {
-    if (a % 3 === 0) {
-        return "Fizz";
-    }
-
-    return a.toString();
-}
-
-describe('sample test-suite', () => {
-    it('sum 1 + 3 should be 4', () => {
-        expect(sum(1, 3)).eql(4);
-    });
-});
-
-describe('fizzbuzz kata tests', () => {
-    it('should return a string of number 1', () => {
-        expect(fizzbuzz(1)).eql("1");
-    });
-});
-
-describe('fizzbuzz kata tests', () => {
-    it('should return a string of number 3', () => {
-        expect(fizzbuzz(3)).eql("Fizz");
+describe('..', () => {
+    it('..', () => {
+       // expect(sum(1, 3)).eql(4);
     });
 });
